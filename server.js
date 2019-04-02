6cabad12ca1cef5ebf967e6a3660934bb72473bf
mightymity <mity_dekaranger@hotmail.com>
2019-04-03 02:02:20 +0700

    first times

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const WebSocket = require('ws');
+const numjs = require('numjs')
+const Dict = require("collections/dict");
 const wss = new WebSocket.Server({ port: 8385}); 
 // สร้าง websockets server ที่ port 4000
 wss.on('connection', function connection(ws) { // สร้าง connection
@@ -18,27 +20,35 @@ wss.on('connection', function connection(ws) { // สร้าง connection
     var positionOfR5X = 4
     var positionOfR5Y = 0
 
+    var routers = {'R1': 999, 'R3': 999, 'R4': 999, 'R5': 999}
+
+    //static variable
+    rssiRefMean = [-58.44]
+    nMean = [1.811]
 
     // รอรับ data อะไรก็ตาม ที่มาจาก client แบบตลอดเวลา
       console.log('received: %s', message);
 
       //test print data after change to new format
       data_withRouter = data_newformat(message)
-
+  
       //extract RSSI from data_withRouter
       var onlyRSSI = data_withRouter.slice(1,data_withRouter.length)
+  
 
       
-      if (data_withRouter[0] == 'R1' && buffer1.length < 200){
+      if (data_withRouter[0] == 'R1 ' && buffer1.length < 200){
         buffer1.push(onlyRSSI)
+        console.log('test1')
       }
-      if (data_withRouter[0] == 'R3' && buffer1.length < 200){
+      if (data_withRouter[0] == 'R3 ' && buffer1.length < 200){
         buffer3.push(onlyRSSI)
+        console.log('test3')
       }
-      if (data_withRouter[0] == 'R4' && buffer1.length < 200){
+      if (data_withRouter[0] == 'R4 ' && buffer1.length < 200){
         buffer4.push(onlyRSSI)
       }
-      if (data_withRouter[0] == 'R5' && buffer1.length < 200){
+      if (data_withRouter[0] == 'R5 ' && buffer1.length < 200){
         buffer5.push(onlyRSSI)
       }
 
@@ -48,6 +58,43 @@ wss.on('connection', function connection(ws) { // สร้าง connection
                         [positionOfR3X, positionOfR3Y],
                         [positionOfR4X, positionOfR4Y],
                         [positionOfR5X, positionOfR5Y]]
+
+      var buffer_length = [buffer1.length,buffer3.length,buffer4.length,buffer5.length]
+      var count = 0
+      for(var i = 0; i < buffer_length.length; ++i){
+        if(buffer_length[i] == 200)
+            count++;
+      }
+      if (count == 4){
+        for (var i = 1; i <= 4; i++){
+            var rssiMean = numjs.mean(buffers[i])
+            var distanceMean = 10 ** ((rssiMean - (rssiRefMean[0])) / (-10 * nMean[0]))
+            var keys = Object.keys(routers)
+            keys.forEach((key)=> {
+              routers[key] = distanceMean
+            } )
+        }
+        var pi = Object.values(routers)
+        var routerUse = [0,1,2,3]
+
+        //Trilateration
+
+        //remove the far one 
+        var far_rasverrypi = pi.index(max(pi))
+
+        var meanPosition = cal_position(routerCoord[routerUse[0]][0],
+          routerCoord[routerUse[0]][1],
+          routerCoord[routerUse[1]][0],
+          routerCoord[routerUse[1]][1],
+          routerCoord[routerUse[2]][0],
+          routerCoord[routerUse[2]][1],
+          float(pi[routerUse[0]]), float(pi[routerUse[1]]),
+          float(pi[routerUse[2]]))
+
+
+
+      }
+
   });
 
 /*
@@ -62,11 +109,12 @@ ws.on('close', function close() {
 //form new_data
 function data_newformat(str) {
   
-  var newform = str.split(" :");
+  var newform = str.split(":");
   return newform
   }
 //trilateration
 function cal_position(x1, y1, x2, y2, x3, y3, distance_from_r1, distance_from_r2, distance_from_r3){
+  console.log('log')
   var a = (-2.0 * x1) + (2.0 * x2)
   var b = (-2.0 * y1) + (2.0 * y2)
   var c = math.pow(distance_from_r1, 2) - math.pow(distance_from_r2, 2) - math.pow(x1, 2) + math.pow(x2, 2) - math.pow(y1, 2) + math.pow(y2, 2)
--- socketServer.py
+++ socketServer.py
@@ -18,11 +18,6 @@ class Server:
         self.buffer4 = []
         self.buffer5 = []
         self.routers = {'R1': 999, 'R3': 999, 'R4': 999, 'R5': 999}
-        '''
-        self.c1routers = {'R1': 999, 'R3': 999, 'R4': 999, 'R5': 999}
-        self.c2routers = {'R1': 999, 'R3': 999, 'R4': 999, 'R5': 999}
-        self.c3routers = {'R1': 999, 'R3': 999, 'R4': 999, 'R5': 999}
-        '''
         # initial position of each Receiver
         self.positionOfR1X = 0
         self.positionOfR1Y = 0
@@ -36,13 +31,14 @@ class Server:
         self.positionOfR5X = 4
         self.positionOfR5Y = 0
         
-        start_server = websockets.serve(self.hello, '10.94.6.158', 8385)
+        start_server = websockets.serve(self.hello, '10.94.13.155', 8385)
         loop = asyncio.get_event_loop()
         loop.run_until_complete(start_server)
         loop.run_forever()
         
     #send data to app
     async def hello(self, websocket, path):
+        print('hello')
         data = await websocket.recv()
         print(data)
         if data == 'app':
@@ -55,11 +51,7 @@ class Server:
         #rssi of d0 = 1m
         rssiRefMean = [-58.44]
         nMean = [1.811]
-        '''
-        ######## cluster model ########
-        rssiRefMeanCluster = [[-48.3787], [-52.7188], [-66.2946]]  # [[c1 angle0],[c2 angle0],[c3 angle0]]
-        nMeanCluster = [[1.8128], [2.6148], [1.9121]]
-        '''
+
         output = ast.literal_eval(sia[1][1:]) #20 sample data
 
 
@@ -95,39 +87,12 @@ class Server:
             # check which pi receives the lowest distance (highest RSSI)
             self.nearestRouter = list(self.routers.keys())[pi.index(min(pi))]
             print('Nearest Router:', self.nearestRouter)
-            '''
-            ###########################################################################################
-            ############################### for kmeans 200 samples ####################################
-            ###########################################################################################
-            for j in range(len(self.buffers)):
-                clusters = self.kmeansClustering(self.buffers[j])
-                for i in range(len(clusters)):
-                    rssiMean = numpy.mean(clusters[i])
-                    print('cluster', i + 1, rssiMean)
-                    distanceMean = 10 ** ((rssiMean - (rssiRefMeanCluster[i][0])) / (-10 * nMeanCluster[i][0]))
-                    if (i == 0):
-                        self.c1routers[list(self.routers.keys())[j]] = distanceMean
-                        print("c1 {}".format(self.c1routers))
-                    elif (i == 1):
-                        self.c2routers[list(self.routers.keys())[j]] = distanceMean
-                        print("c2 {}".format(self.c2routers))
-                    else:
-                        self.c3routers[list(self.routers.keys())[j]] = distanceMean
-                        print("c3 {}".format(self.c3routers))
-'''
             ################################################################################
             ################################## Trilateration ###############################
             ################################################################################
             pi = list(self.routers.values())
             routerUse = [0, 1, 2, 3]
-'''
-            c1pi = list(self.c1routers.values())
-            c2pi = list(self.c2routers.values())
-            c3pi = list(self.c3routers.values())
-            routerUseC1 = [0, 1, 2, 3];
-            routerUseC2 = [0, 1, 2, 3];
-            routerUseC3 = [0, 1, 2, 3];
-'''
+
 
             ################################################################################
             ########################## Moving average trilat ###############################
@@ -151,189 +116,7 @@ class Server:
             print('Mov Avg list2:', self.buffers[routerUse[1]])
             print('Mov Avg list3:', self.buffers[routerUse[2]])
             print('Mov Avg RESULT:', self.meanPosition)
-'''
-            ####################################################################################
-            ######################### kmeans 200 sample trilateration ##########################
-            ####################################################################################
-            ma = c1pi.index(max(c1pi))
-            print('################## Cluster1 ###################')
-            print('remove router index:', ma)
-            routerUseC1.remove(ma)
-            print('C1 list1:', self.buffers[routerUseC1[0]])
-            print('C1 list2:', self.buffers[routerUseC1[1]])
-            print('C1 list3:', self.buffers[routerUseC1[2]])
-            print('C1 RESULT:',
-                  self.calculate_position(routerCoord[routerUseC1[0]][0], routerCoord[routerUseC1[0]][1],
-                                          routerCoord[routerUseC1[1]][0], routerCoord[routerUseC1[1]][1],
-                                          routerCoord[routerUseC1[2]][0], routerCoord[routerUseC1[2]][1],
-                                          float(c1pi[routerUseC1[0]]), float(c1pi[routerUseC1[1]]),
-                                          float(c1pi[routerUseC1[2]])))
 
-            ma = c2pi.index(max(c2pi))
-            print('################## Cluster2 ###################')
-            print('remove router index:', ma)
-            routerUseC2.remove(ma)
-            print('C2 list1:', self.buffers[routerUseC2[0]])
-            print('C2 list2:', self.buffers[routerUseC2[1]])
-            print('C2 list3:', self.buffers[routerUseC2[2]])
-            print('C2 RESULT:',
-                  self.calculate_position(routerCoord[routerUseC2[0]][0], routerCoord[routerUseC2[0]][1],
-                                          routerCoord[routerUseC2[1]][0], routerCoord[routerUseC2[1]][1],
-                                          routerCoord[routerUseC2[2]][0], routerCoord[routerUseC2[2]][1],
-                                          float(c2pi[routerUseC2[0]]), float(c2pi[routerUseC2[1]]),
-                                          float(c2pi[routerUseC2[2]])))
-
-            ma = c3pi.index(max(c3pi))
-            print('################## Cluster3 ###################')
-            print('remove router index:', ma)
-            routerUseC3.remove(ma)
-            print('C3 list1:', self.buffers[routerUseC3[0]])
-            print('C3 list2:', self.buffers[routerUseC3[1]])
-            print('C3 list3:', self.buffers[routerUseC3[2]])
-            print('C3 RESULT:',
-                  self.calculate_position(routerCoord[routerUseC3[0]][0], routerCoord[routerUseC3[0]][1],
-                                          routerCoord[routerUseC3[1]][0], routerCoord[routerUseC3[1]][1],
-                                          routerCoord[routerUseC3[2]][0], routerCoord[routerUseC3[2]][1],
-                                          float(c3pi[routerUseC3[0]]), float(c3pi[routerUseC3[1]]),
-                                          float(c3pi[routerUseC3[2]])))
-
-
-            ###########################################################################################
-            ############################### filter only distance <= sqrt(20) #################################
-            ###########################################################################################
-            movAvgDist = list(self.routers.values())
-            c1Dist = list(self.c1routers.values())
-            c2Dist = list(self.c2routers.values())
-            c3Dist = list(self.c3routers.values())
-
-            for i in range(len(movAvgDist)):
-                if(movAvgDist[i] > math.sqrt(20) and movAvgDist[i] != max(movAvgDist)):
-                    if(i==0):
-                        self.routers['R1'] = math.sqrt(20)
-                    elif (i == 1):
-                        self.routers['R3'] = math.sqrt(20)
-                    elif (i == 2):
-                        self.routers['R4'] = math.sqrt(20)
-                    else:
-                        self.routers['R5'] = math.sqrt(20)
-            for i in range(len(c1Dist)):
-                if(c1Dist[i] > math.sqrt(20) and c1Dist[i] != max(c1Dist)):
-                    if(i==0):
-                        self.c1routers['R1'] = math.sqrt(20)
-                    elif (i == 1):
-                        self.c1routers['R3'] = math.sqrt(20)
-                    elif (i == 2):
-                        self.c1routers['R4'] = math.sqrt(20)
-                    else:
-                        self.c1routers['R5'] = math.sqrt(20)
-            for i in range(len(c2Dist)):
-                if(c2Dist[i] > math.sqrt(20) and c2Dist[i] != max(c2Dist)):
-                    if(i==0):
-                        self.c2routers['R1'] = math.sqrt(20)
-                    elif (i == 1):
-                        self.c2routers['R3'] = math.sqrt(20)
-                    elif (i == 2):
-                        self.c2routers['R4'] = math.sqrt(20)
-                    else:
-                        self.c2routers['R5'] = math.sqrt(20)
-            for i in range(len(c3Dist)):
-                if(c3Dist[i] > math.sqrt(20) and c3Dist[i] != max(c3Dist)):
-                    if(i==0):
-                        self.c3routers['R1'] = math.sqrt(20)
-                    elif (i == 1):
-                        self.c3routers['R3'] = math.sqrt(20)
-                    elif (i == 2):
-                        self.c3routers['R4'] = math.sqrt(20)
-                    else:
-                        self.c3routers['R5'] = math.sqrt(20)
-
-            ################################################################################
-            ################################## Trilateration (bound) ###############################
-            ################################################################################
-            pi = list(self.routers.values())
-            routerUse = [0, 1, 2, 3];
-
-            c1pi = list(self.c1routers.values())
-            c2pi = list(self.c2routers.values())
-            c3pi = list(self.c3routers.values())
-            routerUseC1 = [0, 1, 2, 3];
-            routerUseC2 = [0, 1, 2, 3];
-            routerUseC3 = [0, 1, 2, 3];
-
-            ################################################################################
-            ########################## Moving average trilat (bound) ###############################
-            ################################################################################
-            mi = pi.index(max(pi))
-            print('################## Moving Average (bound) ###################')
-            print('remove router index:', mi)
-            routerUse.remove(mi)
-            self.meanPosition = self.calculate_position(routerCoord[routerUse[0]][0],
-                                                        routerCoord[routerUse[0]][1],
-                                                        routerCoord[routerUse[1]][0],
-                                                        routerCoord[routerUse[1]][1],
-                                                        routerCoord[routerUse[2]][0],
-                                                        routerCoord[routerUse[2]][1],
-                                                        float(pi[routerUse[0]]), float(pi[routerUse[1]]),
-                                                        float(pi[routerUse[2]]))
-            print('Mov Avg list1:', self.buffers[routerUse[0]])
-            print('Mov Avg list2:', self.buffers[routerUse[1]])
-            print('Mov Avg list3:', self.buffers[routerUse[2]])
-            print('Mov Avg RESULT (bound):', self.meanPosition)
-
-            ####################################################################################
-            ######################### kmeans 200 sample trilateration (bound)##########################
-            ####################################################################################
-            ma = c1pi.index(max(c1pi))
-            print('################## Cluster1 (bound) ###################')
-            print('remove router index:', ma)
-            routerUseC1.remove(ma)
-            print('C1 list1:', self.buffers[routerUseC1[0]])
-            print('C1 list2:', self.buffers[routerUseC1[1]])
-            print('C1 list3:', self.buffers[routerUseC1[2]])
-            print('C1 RESULT (bound):',
-                  self.calculate_position(routerCoord[routerUseC1[0]][0], routerCoord[routerUseC1[0]][1],
-                                          routerCoord[routerUseC1[1]][0], routerCoord[routerUseC1[1]][1],
-                                          routerCoord[routerUseC1[2]][0], routerCoord[routerUseC1[2]][1],
-                                          float(c1pi[routerUseC1[0]]), float(c1pi[routerUseC1[1]]),
-                                          float(c1pi[routerUseC1[2]])))
-
-            ma = c2pi.index(max(c2pi))
-            print('################## Cluster2 (bound) ###################')
-            print('remove router index:', ma)
-            routerUseC2.remove(ma)
-            print('C2 list1:', self.buffers[routerUseC2[0]])
-            print('C2 list2:', self.buffers[routerUseC2[1]])
-            print('C2 list3:', self.buffers[routerUseC2[2]])
-            print('C2 RESULT (bound):',
-                  self.calculate_position(routerCoord[routerUseC2[0]][0], routerCoord[routerUseC2[0]][1],
-                                          routerCoord[routerUseC2[1]][0], routerCoord[routerUseC2[1]][1],
-                                          routerCoord[routerUseC2[2]][0], routerCoord[routerUseC2[2]][1],
-                                          float(c2pi[routerUseC2[0]]), float(c2pi[routerUseC2[1]]),
-                                          float(c2pi[routerUseC2[2]])))
-
-            ma = c3pi.index(max(c3pi))
-            print('################## Cluster3 (bound) ###################')
-            print('remove router index:', ma)
-            routerUseC3.remove(ma)
-            print('C3 list1:', self.buffers[routerUseC3[0]])
-            print('C3 list2:', self.buffers[routerUseC3[1]])
-            print('C3 list3:', self.buffers[routerUseC3[2]])
-            print('C3 RESULT (bound):',
-                  self.calculate_position(routerCoord[routerUseC3[0]][0], routerCoord[routerUseC3[0]][1],
-                                          routerCoord[routerUseC3[1]][0], routerCoord[routerUseC3[1]][1],
-                                          routerCoord[routerUseC3[2]][0], routerCoord[routerUseC3[2]][1],
-                                          float(c3pi[routerUseC3[0]]), float(c3pi[routerUseC3[1]]),
-                                          float(c3pi[routerUseC3[2]])))
-            ###### clear buffers #######
-            self.buffer1 = []
-            self.buffer3 = []
-            self.buffer4 = []
-            self.buffer5 = []
-
-
-        #############################################################
-
-        '''
     def formatData(self, data):
         output = data.split(' :')
         return output
@@ -359,34 +142,6 @@ class Server:
 
         return pos
 
-'''
-    def kmeansClustering(self, y):
-        y = numpy.array(y)
-        x = numpy.array([1] * len(y))
-        data = numpy.array(list(zip(x, y))).reshape(len(x), 2)
-
-        # KMeans algorithm
-        K = 3
-        kmeans_model = KMeans(n_clusters=K).fit(data)
-        # clusters
-        c1 = []
-        c2 = []
-        c3 = []
-        for i, l in enumerate(kmeans_model.labels_):
-            if (l == 0):
-                c1.append(y[i])
-            elif (l == 1):
-                c2.append(y[i])
-            else:
-                c3.append(y[i])
-
-        order = [c1, c2, c3]
-        c3 = order.pop(order.index(min(order)))
-        c1 = order.pop(order.index(max(order)))
-        c2 = order[0]
-
-        return [c1, c2, c3]
-'''
 
     async def send(self, websocket, path):
         position = ''
